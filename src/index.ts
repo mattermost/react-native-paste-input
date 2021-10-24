c1948e4961a12db867648269c99a7d9cebb0130e
Elias Nahum <nahumhbl@gmail.com>
2021-10-24 12:14:34 -0300

    fix: ios export

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ let PasteInput: React.ForwardRefExoticComponent<
 if (Platform.OS === 'android') {
     PasteInput = require('./android');
 } else {
-    PasteInput = require('./ios');
+    PasteInput = require('./ios').default;
 }
 
 export default PasteInput;
